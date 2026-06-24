fe7e51a88e00420bf46900b49c818b94a3ad4b41
Mohana Priya k <mohanapriyak624@gmail.com>
2026-06-24 10:30:22 +0530

    Add files via upload

---
+++ Week1/Mockito_excercise/mockito-excercises/src/test/java/com/mohana/mockito/MyServiceTest.java
@@ -0,0 +1,23 @@
+package com.mohana.mockito;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyServiceTest {
+
+    @Test
+    public void testExternalApi() {
+
+        ExternalApi mockApi = mock(ExternalApi.class);
+
+        when(mockApi.getData()).thenReturn("Mock Data");
+
+        MyService service = new MyService(mockApi);
+
+        String result = service.fetchData();
+
+        assertEquals("Mock Data", result);
+    }
+}
